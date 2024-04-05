6e3ba15aff82a7eb30c3994a0b899c3aa2f26411
fluffelpuff <ipncore@proton.me>
2024-04-05 13:48:24 +0200

    .

--- apiservices/localgrpc/localgrpcservice/rpc_get_vm_details.go
+++ apiservices/localgrpc/localgrpcservice/rpc_get_vm_details.go
@@ -2,10 +2,17 @@ package localgrpcservice
 
 import (
 	"context"
+	"fmt"
 	"vnh1/localgrpcproto"
 )
 
 func (s *CliGrpcServer) GetVMDetails(ctx context.Context, vmDetailParms *localgrpcproto.VmDetailsParms) (*localgrpcproto.VmDetailsResponse, error) {
+	switch vmDetailParms.Value.(type) {
+	case localgrpcproto.VmDetailsParms_Id:
+	case localgrpcproto.VmDetailsParms_Name:
+	default:
+		return nil, fmt.Errorf("invalid 'get vm details' parameter vm id/name")
+	}
 
 	// Die Daten werden zurückgesendet
 	return &localgrpcproto.VmDetailsResponse{}, nil
